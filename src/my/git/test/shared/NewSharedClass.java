7b699fe3f17efb17af82961b37fbfebe57bcdfdd
ANazarov <ANazarov@MININT-9O1BMI8.ntv-tv.ru>
2014-04-22 17:16:56 +0400

    new Shared class

---
+++ src/my/git/test/shared/NewSharedClass.java
@@ -0,0 +1,13 @@
+package my.git.test.shared;
+
+public class NewSharedClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
